628860d6a841ead74c2c7e999a621463b5678522
Akif Yalcin Ozhabes <ao294@en-cs-totient-01.coecis.cornell.edu>
2015-09-22 16:50:55 -0400

    workq synchronization

--- 2015-09-22/workq/workq.c
+++ 2015-09-22/workq/workq.c
@@ -152,8 +152,11 @@ void workq_put(workq_t* workq, void* data)
 {
     task_t* task = (task_t*) malloc(sizeof(task_t));
     task->data = data;
+    workq_lock(workq);
     task->next = workq->tasks;
     workq->tasks = task;
+    workq_signal(workq);
+    workq_unlock(workq);
 }
 
 
@@ -165,12 +168,16 @@ void workq_put(workq_t* workq, void* data)
 void* workq_get(workq_t* workq)
 {
     void* result = NULL;
+    workq_lock(workq);
+    workq_wait(workq);
     if (workq->tasks) {
         task_t* task = workq->tasks;
         result = task->data;
         workq->tasks = task->next;
         free(task);
     }
+    workq_signal(workq);
+    workq_unlock(workq);
     return result;
 }
 
@@ -183,7 +190,10 @@ void* workq_get(workq_t* workq)
  */
 void workq_finish(workq_t* workq)
 {
+    workq_lock(workq);
     workq->done = 1;
+    workq_broadcast(workq);
+    workq_unlock(workq);
 }
 
 
